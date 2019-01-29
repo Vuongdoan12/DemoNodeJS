58fa35601d1eaeca382053d131d4a0dcb2ad8c12
VuongDH <mac@MACs-MacBook-Pro.local>
2019-01-29 14:53:47 +0700

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+// dev.js - don't commit this
+
+module.exports = {
+    googleClientID: '417195368187-88302d7ctp2098p8ncmma74oua80e3gd.apps.googleusercontent.com',
+    googleClientSecret: 'vxklOP7KqaWHrhSbHjqmDiDO',
+    mongoURI: 'mongodb://vuongdoan12:Doanhavuong12@ds213665.mlab.com:13665/emaily-dev-demo',
+    cookieKey: 'agfjhafajgffjkahkahkfhkfhkhfkhfdsjfhfdhskahkshhksha'
+};
